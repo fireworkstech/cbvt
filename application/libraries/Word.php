0c244e2052b9a5e01bc683d84cce2a2819a0b625
Taha <tahahameed1@gmail.com>
2015-11-26 11:42:55 +0500

    barcode generator

--- application/libraries/Word.php
+++ application/libraries/Word.php
@@ -2,7 +2,8 @@
  
 // require_once APPPATH."/third_party/PhpWord.php"; 
 // require_once(APPPATH."/third_party/PhpWord.php");
- 
+// $ci = &get_instance();
+// $ci->load->file('third_party/PhpWord.php'); 
 // class Word extends PhpWord { 
 //     public function __construct() { 
 //         parent::__construct(); 
